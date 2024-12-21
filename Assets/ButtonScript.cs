31cce4e9f256b6482c567d21f18b45b5c40dcf7c
ThomasNewton03 <2668168n@student.gla.ac.uk>
2024-12-21 18:33:31 +0000

    poke interaction not complete yet but is underway

--- Assets/ButtonScript.cs
+++ Assets/ButtonScript.cs
@@ -1,18 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ButtonScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Button nextTaskButton;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void OnClick(){
+        nextTaskButton.onClick.Invoke();
     }
 }
